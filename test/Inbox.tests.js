65849d0e4d6a4430da4e4775479ef6cb6cdae06e
Jason Kwong <jasonkwong11@gmail.com>
2018-02-11 17:46:49 -0500

    test contract deploy

--- test/Inbox.tests.js
+++ test/Inbox.tests.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
 /* test mocha test
 class Car {
@@ -32,4 +33,23 @@ describe('Car', () => {
 
 */
 
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+
+  // Use one of those accounts to deploy
+  // the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ from: accounts[0], gas: '1000000' })
+})
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    console.log(inbox);
+  });
+});
 
