d5c5aea09ab4d16d9931caba032b0d1da49b9051
Jason Kwong <jasonkwong11@gmail.com>
2018-02-11 21:23:00 -0500

    add ethereum tests

--- test/Inbox.tests.js
+++ test/Inbox.tests.js
@@ -1,7 +1,9 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+
+const provider = ganache.provider()
+const web3 = new Web3(provider);
 const { interface, bytecode } = require('../compile');
 
 /* test mocha test
@@ -45,11 +47,29 @@ beforeEach(async () => {
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode, arguments: ['Hi there!'] })
     .send({ from: accounts[0], gas: '1000000' })
+
+  inbox.setProvider(provider);
 })
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
-    console.log(inbox);
+    assert.ok(inbox.options.address)
   });
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there!')
+  });
+
+  it('can set a new message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there!')
+
+    await inbox.methods.setMessage('Good morning Jason')
+      .send({ from: accounts[0] });
+
+    const newMessage = await inbox.methods.message().call();
+    assert.equal(newMessage, 'Good morning Jason')
+  })
 });
 
